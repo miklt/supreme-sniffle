31abbea666f80b0956f21ad3a4789dedc9497350
michelet <michelet@usp.br>
2021-10-16 21:45:36 -0300

    dev

---
+++ mapping/transfer.ts
@@ -0,0 +1,15 @@
+import {Exchange} from "../generated/schema";
+import { Transfer } from "../generated/Transfer/USDT";
+
+function handleTransfer(event: Transfer): void { 
+   let transfer =  Exchange.load(event.transaction.hash.toHex());
+   if (transfer == null){
+    transfer = new Exchange(event.transaction.hash.toHex());
+   }
+   transfer.from = event.params.from.toHex();
+   transfer.to = event.params.to.toHex();
+   transfer.amount = event.params.value;
+   transfer.block = event.block.number;
+   transfer.save();
+   
+}
